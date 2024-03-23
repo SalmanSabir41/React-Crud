6ac652145e6ac13b7cda6f1b33cfb964544f72fd
sabir <ss2885092@gmail.com>
2024-03-23 23:23:29 +0530

    add data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
 import './App.css';
+import { CrudData } from './CrudData';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [age, setAge] = useState('')
+  const [isId, setId] = useState('')
+  const [isUpdate, setIsUpdate] = useState(false)
+
+  useEffect(() => {
+    setData(CrudData)
+  }, []);
+
+  const handleEdit = (id) => {
+    const dt = data.filter(item => item.id === id);
+    setId(dt[0].id)
+    if(dt !== undefined){
+      setIsUpdate(true)
+      setFirstName(dt[0].firstName);
+      setLastName(dt[0].lastName);
+      setAge(dt[0].age);
+    }
+  }
+
+  const handleDelete = (id) => {
+    if (id > 0) {
+      if (window.confirm("are you sure to delete this item?")) {
+        const dt = data.filter(item => item.id !== id);
+        setData(dt)
+      }
+    }
+  }
+
+  const handleAdd = (e) => {
+   e.preventDefault();
+   const dt = [...data];
+   const newObject = {
+    id: CrudData.length + 1,
+    firstName: firstName,
+    lastName: lastName,
+    age: age
+   }
+   dt.push(newObject);
+   setData(dt);
+   handleClear();
+  }
+
+  const handleUpdate = () => {
+    
+    const index = data.findIndex(item => item.id === isId);
+    
+    
+    if (index !== -1) {
+      const updatedData = [...data]; 
+      updatedData[index] = {   
+        id: isId,
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+      };
+      setData(updatedData); 
+      handleClear(); 
+    }
+  }
+  
+
+  const handleClear = () => {
+    setFirstName('');
+    setLastName('');
+    setAge('');
+   setIsUpdate(false)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex', justifyContent: 'center', padding:"10px", marginBottom:"10px", backgroundColor:"black", color:"white", alignItems:"center"  }}>
+        <div>
+          <label>First Name:
+            <input type='text' placeholder='First Name' onChange={(e)=> setFirstName(e.target.value)} value={firstName} />
+          </label>
+        </div>
+        <div>
+          <label>Last Name:
+            <input type='text' placeholder='Last Name' onChange={(e)=> setLastName(e.target.value)} value={lastName}/>
+          </label>
+        </div>
+        <div>
+          <label> Age:
+            <input type='number' placeholder='Age' onChange={(e)=> setAge(e.target.value)} value={age}/>
+          </label>
+        </div>
+        <div style={{padding:"0 10px 0 10px"}} className='btns'>
+          {
+            !isUpdate  ?
+            <button  className='btn btn-primary' onClick={(e) => handleAdd(e)}>Add</button>
+            :
+            <button className='btn btn-primary' onClick={() => handleUpdate()}>Update</button>
+          }
+          <button className='btn btn-danger' onClick={() => handleClear()}>Clear</button>
+        </div>
+      </div>
+      <table className='table table-hover'>
+        <thead>
+        
+          <tr>
+            <td>Id</td>
+            <td>firstName</td>
+            <td>LastName</td>
+            <td>Age</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item, index) => {
+              return (
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                  <td>{item.age}</td>
+                  <td>
+                    <button className='btn btn-primary' onClick={() => handleEdit(item.id)}>Edit</button>&nbsp;
+                    <button className='btn btn-danger' onClick={() => handleDelete(item.id)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+        
+      </table>
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/CrudData.jsx
@@ -0,0 +1,23 @@
+export const CrudData = [
+    {
+        id : 1,
+        firstName : 'MS',
+        lastName : 'Dhoni',
+        age : 34
+
+    },
+    {
+        id : 2,
+        firstName : 'Virat',
+        lastName : 'Kohli',
+        age : 32
+
+    },
+    {
+        id : 3,
+        firstName : 'Rohit',
+        lastName : 'Sharma',
+        age : 36 
+
+    }
+]
